2ecf8d59b6ffebafda7bf90bcca47c0872cda109
Tim BERNIC <nicolet.t@hotmail.com>
2024-11-15 14:50:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const sidenav = document.getElementById("mySideNav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+function closeNav() {
+  sidenav.classList.remove("active");
+}
